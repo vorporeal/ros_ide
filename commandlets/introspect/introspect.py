cfb2e34d4bfb0ca561c9ce72b0920fe546d4d162
David Stern <vorporeal@gmail.com>
2011-09-29 18:29:33 -0400

    Cleaned up code in introspect.py.

--- commandlets/introspect/introspect.py
+++ commandlets/introspect/introspect.py
@@ -8,12 +8,24 @@ from ros import rosnode
 from ros import rosgraph
 from rosgraph import masterapi
 
+status = None
+pub_topics = None
+
 # Get the type of a given topic.
-def topic_type(t, pub_topics):
+def topic_type(t):
         matches = [t_type for t_name, t_type in pub_topics if t_name == t]
         if matches:
             return matches[0]
         return None
+
+def topic_data(t):
+    ret = {}
+    ret['name'] = t.rsplit('/', 1)[1]
+    ret['id'] = t
+    ret['type'] = topic_type(t)
+    ret['connections'] = []
+
+    return ret
 
 if __name__ == '__main__':
     # Get the names of the running nodes.
@@ -23,8 +35,6 @@ if __name__ == '__main__':
     master = masterapi.Master('/ride_introspect')
 
     # Get the "node state" of the system.
-    state = None
-    pub_topics = None
     try:
         state = master.getSystemState()
         pub_topics = master.getPublishedTopics('')
@@ -37,9 +47,15 @@ if __name__ == '__main__':
     for n in nodes:
         name = n.rsplit('/', 1)[1]
 
-        outputs = [{'name': t.rsplit('/', 1)[1], 'id': t, 'type': topic_type(t, pub_topics), 'connections': []} for t, l in state[0] + state[2] if n in l]
-        inputs = [{'name': t.rsplit('/', 1)[1], 'id': t, 'type': topic_type(t, pub_topics), 'connections': []} for t, l in state[1] if n in l]
+        outputs = [topic_data(t) for t, l in state[0] + state[2] if n in l]
+        inputs = [topic_data(t) for t, l in state[1] if n in l]
         
-        data['nodes'].append({'name':name, 'id':n, 'outputs':outputs, 'inputs':inputs})
+        data['nodes'].append({'name':name \
+                             ,'id':n \
+                             ,'outputs':outputs \
+                             ,'inputs':inputs \
+                             ,'x': 0 \
+                             ,'y': 0 \
+                             })
 
     print(json.dumps(data))
